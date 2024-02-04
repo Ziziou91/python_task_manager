c8b87d3ca17f99fb1859a16ae5dd6dd559e3fa8f
Josh Gray <joshgray91@gmail.com>
2024-02-04 11:36:45 +0000

    implement add_task() functionality

--- task_manager.py
+++ task_manager.py
@@ -12,21 +12,74 @@ from datetime import datetime, date
 DATETIME_STRING_FORMAT = "%Y-%m-%d"
 
 
-def reg_user():
+def reg_user() -> None:
     '''Add a new user to the user.txt file'''
     new_username = input("New Username: ")
+    # TODO - Should test if someone with that username already exists.
     new_password = input("New Password: ")
     confirm_password = input("Confirm Password: ")
     if new_password == confirm_password:
         print("New user added")
         username_password[new_username] = new_password
-        with open("user.txt", "w", encoding="UTF-8") as out_file:
-            user_data = []
-            for k in username_password:
-                user_data.append(f"{k};{username_password[k]}")
-            out_file.write("\n".join(user_data))
+        write_user(username_password)
     else:
         print("Passwords do no match")
+
+def write_user(username_password: dict) -> None:
+    with open("user.txt", "w", encoding="UTF-8") as out_file:
+        user_data = []
+        for k in username_password:
+            user_data.append(f"{k};{username_password[k]}")
+        out_file.write("\n".join(user_data))    
+
+
+def add_task() -> None:
+    """Allow a user to add a new task to task.txt file."""
+    # TODO - refactor to break out current date and append logic
+    task_username = input("Name of person assigned to task: ")
+    if task_username not in username_password.keys():
+        # TODO - requires more comprehensive logic.
+        print("User does not exist. Please enter a valid username")
+        # continue
+    task_title = input("Title of Task: ")
+    task_description = input("Description of Task: ")
+    while True:
+        try:
+            task_due_date = input("Due date of task (YYYY-MM-DD): ")
+            due_date_time = datetime.strptime(task_due_date, DATETIME_STRING_FORMAT)
+            break
+
+        except ValueError:
+            print("Invalid datetime format. Please use the format specified")
+    # Then get the current date.
+    curr_date = date.today()
+    ''' Add the data to the file task.txt and
+        Include 'No' to indicate if the task is complete.'''
+    new_task = {
+        "username": task_username,
+        "title": task_title,
+        "description": task_description,
+        "due_date": due_date_time,
+        "assigned_date": curr_date,
+        "completed": False
+    }
+
+    task_list.append(new_task)
+    with open("tasks.txt", "w") as task_file:
+        task_list_to_write = []
+        for t in task_list:
+            str_attrs = [
+                t['username'],
+                t['title'],
+                t['description'],
+                t['due_date'].strftime(DATETIME_STRING_FORMAT),
+                t['assigned_date'].strftime(DATETIME_STRING_FORMAT),
+                "Yes" if t['completed'] else "No"
+            ]
+            task_list_to_write.append(";".join(str_attrs))
+        task_file.write("\n".join(task_list_to_write))
+    print("Task successfully added.")
+            
 
 
 # Create tasks.txt if it doesn't exist
@@ -109,58 +162,8 @@ e - Exit
 
 
     elif menu == 'a':
-        '''Allow a user to add a new task to task.txt file
-            Prompt a user for the following: 
-             - A username of the person whom the task is assigned to,
-             - A title of a task,
-             - A description of the task and 
-             - the due date of the task.'''
-        task_username = input("Name of person assigned to task: ")
-        if task_username not in username_password.keys():
-            print("User does not exist. Please enter a valid username")
-            continue
-        task_title = input("Title of Task: ")
-        task_description = input("Description of Task: ")
-        while True:
-            try:
-                task_due_date = input("Due date of task (YYYY-MM-DD): ")
-                due_date_time = datetime.strptime(task_due_date, DATETIME_STRING_FORMAT)
-                break
-
-            except ValueError:
-                print("Invalid datetime format. Please use the format specified")
-
-
-        # Then get the current date.
-        curr_date = date.today()
-        ''' Add the data to the file task.txt and
-            Include 'No' to indicate if the task is complete.'''
-        new_task = {
-            "username": task_username,
-            "title": task_title,
-            "description": task_description,
-            "due_date": due_date_time,
-            "assigned_date": curr_date,
-            "completed": False
-        }
-
-        task_list.append(new_task)
-        with open("tasks.txt", "w") as task_file:
-            task_list_to_write = []
-            for t in task_list:
-                str_attrs = [
-                    t['username'],
-                    t['title'],
-                    t['description'],
-                    t['due_date'].strftime(DATETIME_STRING_FORMAT),
-                    t['assigned_date'].strftime(DATETIME_STRING_FORMAT),
-                    "Yes" if t['completed'] else "No"
-                ]
-                task_list_to_write.append(";".join(str_attrs))
-            task_file.write("\n".join(task_list_to_write))
-        print("Task successfully added.")
-
-
+        add_task()
+    
     elif menu == 'va':
         '''Reads the task from task.txt file and prints to the console in the 
            format of Output 2 presented in the task pdf (i.e. includes spacing
